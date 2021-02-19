dd53b46ab6384c6530ec76850cb01f074d522cb4
joal <jokke34@gmail.com>
2021-02-19 13:22:58 +0100

    Finished 2nd server

--- 2._Second_Server/app.js
+++ 2._Second_Server/app.js
@@ -1,15 +1,19 @@
 const app = require("express")();
 
+
 app.get("/time", (req, res) => {
-    
+    const currentDate = new Date();
+    res.send({"time": currentDate.getHours()+":"+currentDate.getMinutes()+":"+currentDate.getSeconds()});
 });
 
 app.get("/date", (req, res) => {
-
+    const currentDate = new Date();
+    res.send({"date": currentDate.getDate()});
 });
 
 app.get("/month", (req, res) => {
-
+    const currentDate = new Date();
+    res.send({"month": currentDate.getMonth()+1});
 });
 
 app.listen(8080);
