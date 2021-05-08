c7e96425c38c1150bfbdce5d69b28fe3eb038e94
JoakimGit <jokke34@gmail.com>
2021-05-08 23:23:52 +0200

    Further work on schedule

--- 5._Nodefolio/public/schedule/schedule.js
+++ 5._Nodefolio/public/schedule/schedule.js
@@ -1,8 +1,26 @@
-$('#date').datetimepicker({
-    inline: true,
-    weeks: true,
-    allowTimes: [
-        '10:00', '11:00', '12:00', '13:00',  
-        '14:00', '15:00', '16:00', '17:00'
-    ]
-});
+$(document).ready(function(){
+
+    $('#confirmation').hide()
+
+    $('#date').datetimepicker({
+        inline: true,
+        weeks: true,
+        allowTimes: [
+            '10:00', '11:00', '12:00', '13:00',  
+            '14:00', '15:00', '16:00', '17:00'
+        ]
+    });
+
+    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+
+    const months = ['January', 'February', 'March', 'April', 'May', 'June',
+        'July', 'August', 'September', 'October', 'November', 'December']
+
+    $('#submit').click(function(){
+        const date = $('#date').datetimepicker("getValue");
+        const dateFormatted = `You have scheduled a meeting ${days[date.getDay()]} ${months[date.getMonth()]}
+            ${date.getDate()} ${date.getFullYear()} at ${date.getHours()}:00`;
+        $('#confirmation').text(dateFormatted);
+        $('#confirmation').show();
+    });
+}); 
